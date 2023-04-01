2fd8a9395ebc49c4333cf507b4e5eb90afec9f6a
Daniel Armstrong <33160370+armstrongdj@users.noreply.github.com>
2023-03-31 23:18:47 -0700

    remove boilerplate CSS

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from "next/head"
+
+export default function About() {
+    return (
+        <>
+            <Head>
+                <title>CodeOrchard - About</title>
+            </Head>
+            <main>
+                <div>
+                    <p>
+                        Site by Daniel Armstrong (code arborist)
+                    </p>          
+                </div>
+            </main>
+        </>
+        
+    )
+}
---
+++ pages/varieties.js
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+export default function Varieties() {
+    return (
+        <>
+            <Head>
+                <title>CodeOrchard - About</title>
+            </Head>
+            <main>
+                <p>Varieties</p>
+            </main>
+        </>      
+    )
+  }
